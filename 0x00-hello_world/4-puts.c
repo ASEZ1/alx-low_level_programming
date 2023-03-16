a07795cf96f59b901849f3d157d2431a620523e0
ASEZ1 <asmaaezzine96@gmail.com>
2023-03-16 15:33:46 -0700

    Task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#!/bin/bash
+#include <stdio.h>
+/**
+ * main - It prints a message with puts function
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
