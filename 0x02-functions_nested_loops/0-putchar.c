e59f32d327014ee83f3957ebca4cf41cbb69f824
ASEZ1 <asmaaezzine96@gmail.com>
2023-03-21 13:45:02 -0700

    first task

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include"main.h"
+
+/**
+ * main - Entry point
+ *
+ * Description: prints _putchar using putchar prototype
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	char str[] = "_putchar";
+	int a;
+
+	for (a = 0; a < 8; ++a)
+		_putchar(str[a]);
+	_putchar('\n');
+
+	return (0);
+}
