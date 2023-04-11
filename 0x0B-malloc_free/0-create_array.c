0be60f9a6e2aa2b4434b9289080dec30d0d952d0
ASEZ1 <asmaaezzine96@gmail.com>
2023-04-11 15:24:42 -0700

    malloc, free

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * create_array - creates an array of characters and initializes it with a given character.
+ * @size: size of the array.
+ * @ch: character to initialize the array with.
+ *
+ * Return: pointer to the array of characters, or NULL if allocation fails.
+ */
+char *create_array(unsigned int size, char ch)
+{
+	char *arr;
+	unsigned int i;
+
+	if (size == 0)
+		return (NULL);
+
+	arr = malloc(sizeof(char) * size);
+
+	if (arr == NULL)
+		return (NULL);
+
+	for (i = 0; i < size; i++)
+		arr[i] = ch;
+
+	return (arr);
+}
---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strdup - returns a pointer to a newly allocated space in memory.
+ * @s: string.
+ *
+ * Return: pointer of an array of chars
+ */
+char *_strdup(char *s)
+{
+    char *strout;
+    unsigned int i, j;
+
+    if (s == NULL)
+        return (NULL);
+
+    for (i = 0; s[i] != '\0'; i++)
+        ;
+
+    strout = (char *) malloc(sizeof(char) * (i + 1));
+
+    if (strout == NULL)
+        return (NULL);
+
+    for (j = 0; j <= i; j++)
+        strout[j] = s[j];
+
+    return (strout);
+}
