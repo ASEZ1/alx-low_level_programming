93b3739e2e7fa1da211cdddd8901fa47a7895dca
ASEZ1 <asmaaezzine96@gmail.com>
2023-07-10 15:07:59 -0700

    all tasks

---
+++ 0x15-file_io/main.h
@@ -0,0 +1,18 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#include <sys/types.h>
+#include <sys/stat.h>
+
+char _putchar(void);
+ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
+
+#endif
